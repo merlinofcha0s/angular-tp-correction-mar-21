2d1460fef56230b0e5e9ac13255e37ad5116427e
Cyril Casaucau <casaucau.cyril@protonmail.com>
2022-03-14 14:06:22 +0100

    Third day (second half) : Async pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import {Component} from '@angular/core';
-import {Vinyl} from "./vinyl/single-vinyl/vinyl.model";
+import {Vinyl} from "./vinyl/vinyl.model";
 import {VinylService} from "./vinyl/vinyl.service";
 import {UserService} from "./user/user.service";
 import {User} from "./user/user.model";
-import {filter, map, throttle, throwError} from "rxjs";
+import {filter, map, Observable, throttle, throwError} from "rxjs";
+import {HttpResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -14,25 +15,16 @@ export class AppComponent {
   title = 'toto';
 
   vinyls: Array<Vinyl> = [];
-  users: Array<User> | null = [];
+  users: Observable<Array<User> | null>;
 
   errorCallUser = false;
 
   constructor(private vinylService: VinylService, private userService: UserService) {
     this.vinyls = this.vinylService.getAll();
-    this.userService.getAll().pipe(
+    this.users = this.userService.getAll().pipe(
       filter(httpResponse => httpResponse.ok),
       map(httpResponse => httpResponse.body),
       // map(httpResponse => {throw new Error()})
-    ).subscribe({
-      next: users => {
-        this.users = users;
-      },
-      error: (error) => {
-        console.error("Probleme lors de l'appel http " + error);
-        this.errorCallUser = true;
-      },
-      complete: () => console.log('terminate')
-    });
+    );
   }
 }
--- src/app/vinyl/single-vinyl/single-vinyl.component.ts
+++ src/app/vinyl/single-vinyl/single-vinyl.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {Vinyl} from "./vinyl.model";
+import {Vinyl} from "../vinyl.model";
 
 @Component({
   selector: 'app-single-vinyl',
